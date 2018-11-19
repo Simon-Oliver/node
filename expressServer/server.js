09ccad703347bc0290530a9355fb4614837749ce
Simon-Oliver <stauffer.simon@outlook.com>
2018-11-19 22:29:31 +1100

    Added views and partials

--- expressServer/server.js
+++ expressServer/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send("I'm runing");
+  const data = {
+    welcome: 'Hi!!! You have found my website!',
+    currentYear: new Date().getFullYear()
+  };
+  res.render('home', { data });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Whoops!!! Something went wrong!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  const user = {
+    name: 'Simon',
+    hobbies: ['Coding', 'Photography']
+  };
+  res.render('about', { user });
 });
 
 app.listen(3000);
