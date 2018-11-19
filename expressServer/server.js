1a154ef56d8349a3915fcefa3f16373992993c17
Simon-Oliver <stauffer.simon@outlook.com>
2018-11-19 23:36:31 +1100

    Added more routes

--- expressServer/server.js
+++ expressServer/server.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now} ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance');
+});
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   const data = {
-    welcome: 'Hi!!! You have found my website!',
-    currentYear: new Date().getFullYear()
+    welcome: 'Hi!!! You have found my website!'
   };
   res.render('home', { data });
 });
