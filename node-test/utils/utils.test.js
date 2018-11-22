f5a5518cae68b4476b61c918a15e59fc2f4600d3
Simon-Oliver <stauffer.simon@outlook.com>
2018-11-22 18:53:29 +1100

    added testing file

--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,16 +1,43 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(2, 3);
-  if (res !== 5) {
-    throw new Error(`Expected 5 but got ${res}`);
-  }
+
+  expect(res).toBe(5);
+  //   if (res !== 5) {
+  //     throw new Error(`Expected 5 but got ${res}`);
+  //   }
 });
 
 it('should square number', () => {
   const res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9 but got ${res}`);
-  }
+  expect(res)
+    .toBe(9)
+    .toBeA('number');
+});
+
+it('should expect some value', () => {
+  //   expect(12).toNotBe(11);
+  //   expect({ name: 'simon' }).toEqual({ name: 'simon' });
+  //   expect([2, 3, 4]).toInclude(4);
+  expect({
+    name: 'Max Muster',
+    age: 55,
+    location: 'Berlin'
+  }).toInclude({ age: 55 });
+});
+
+it('should include firstName and lastName', () => {
+  const User = {
+    location: 'Berlin',
+    age: 66
+  };
+
+  const name = 'Max Muster';
+  const res = utils.setName(User, name);
+  expect(res)
+    .toInclude({ firstName: 'Max', lastName: 'Muster' })
+    .toBeA('object');
 });
