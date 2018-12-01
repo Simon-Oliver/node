a3ffd5c1a20bf85ee945cbc742fd128248f9ac9f
Simon-Oliver <stauffer.simon@outlook.com>
2018-12-01 13:42:06 +1100

    adde get route

--- nodeToDo/server/server.js
+++ nodeToDo/server/server.js
@@ -10,12 +10,19 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   const todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    completed: req.body.completed
   });
 
   todo
     .save()
     .then(doc => res.send(doc))
+    .catch(e => res.status(400).send(e));
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => res.send({ todos }))
     .catch(e => res.status(400).send(e));
 });
 
